4cda368062b796f785e52cb73daa732c92ea6ef7
Pavel Stupnikov <dp@dpointer.org>
2022-10-15 00:03:00 +0300

    Set a global y offset for trains

--- lib.py
+++ lib.py
@@ -4,3 +4,5 @@ import grf
 
 class Train(grf.Train):
     pass
+
+set_global_train_y_offset = lambda ofs: grf.ComputeParameters(target=0x8e, operation=0x00, if_undefined=False, source1=0xff, source2=0xff, value=ofs)
