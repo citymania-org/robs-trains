bf92d302102398b46ae5cdcd82b6d31a43127abd
Brickblock1 <100433052+Brickblock1@users.noreply.github.com>
2023-11-30 22:18:23 +0100

    Done the important Z4p
    SL ones still to do

--- generate.py
+++ generate.py
@@ -141,18 +141,18 @@ alignment16 = Train(
         '': 'template.png'
     }),
 )
-
+'''
 Train(
     id='alignment_narrow',
     name='alignment narrow',
-    length=16,
-    liveries=make_liveries({
-        '': 'template_narrow_gauge.png'
-    }),
-    #country='sweden',
+    length=8,
+    liveries={
+        '': Livery('alignment_narrow_gauge_short.png')
+    },
+    country='sweden',
     company='na',
     power_type='multi',
-    purchase_sprite_towed_id='x10p',
+    purchase_sprite_towed_id='s_e_X10p_2_sl_car2',
     engine_class=Train.EngineClass.DIESEL,
     sound_effects=modern_diesel_sound,
     track_type=p_gauge,
@@ -172,7 +172,7 @@ Train(
         'Info': 'Test locomotive',
     }),
 )
-'''
+
 # regular trains
 
 # steam locos
@@ -3404,9 +3404,8 @@ s_d_Z4p_1_srj = Train(
     name='SRJ Z4p',
     length=3,
     liveries={
-        'Default': Livery('1947_SE_Z4p_1_1947.png'),
-    #    'NKIJ': '1947_SE_Z4p_2_xxxx.png',
-    #    'DONJ': '1947_SE_Z4p_3_xxxx.png',
+        'Default': Livery('1947_SE_Z4p_1_1947.png', cc_replace=REDBROWN),
+        '2CC': Livery('1947_SE_Z4p_1_1947.png', auto_cc=lib.CC_DEFAULT),
     #    'SL Grey and yellow': '1947_SE_Z4p_4_xxxx.png',
     #    'SL Red': '1947_SE_Z4p_5_xxxx.png',
     },
@@ -3442,6 +3441,41 @@ s_d_Z4p_2_nklj = Train(
     liveries={
         'Default': Livery('1947_SE_Z4p_2_xxxx.png', cc_replace=MAROON, cc2_replace=CREAM),
         '2CC': Livery('1947_SE_Z4p_2_xxxx.png', auto_cc=lib.CC_DEFAULT),
+    },
+    misc_flags=Train.Flags.USE_2CC,
+    country='sweden',
+    company='na',
+    power_type='diesel',
+    purchase_sprite_towed_id='s_p_Co_1',
+    engine_class=Train.EngineClass.DIESEL, 
+    sound_effects=modern_diesel_sound,
+    track_type=p_gauge,
+    max_speed=Train.kmhish(40),
+    power=160,
+    introduction_date=date(1947, 1, 1),
+    vehicle_life=8,
+    model_life=144,
+    climates_available=grf.ALL_CLIMATES,
+    weight=14,
+    tractive_effort_coefficient=79,
+    running_cost_factor=222,
+    cargo_capacity=1,
+    cost_factor=24,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS, # can't be NONE
+    additional_text=grf.fake_vehicle_info({
+        'Info': 'The most common diesel locomotive on narrow gauge lines. The first ones were acquired by SRJ, with other private companies and SJ following suit',
+    }),
+    callbacks={'properties': {'cargo_capacity': 0},}
+)
+
+
+s_d_Z4p_3_donj = Train(
+    id='s_d_Z4p_3_DONJ',
+    name='DONJ Z4p',
+    length=3,
+    liveries={
+        'Default': Livery('1947_SE_Z4p_3_xxxx.png', cc_replace=ORANGE, cc2_replace=DTURQUOISE),
+        '2CC': Livery('1947_SE_Z4p_3_xxxx.png', auto_cc=lib.CC_DEFAULT),
     },
     misc_flags=Train.Flags.USE_2CC,
     country='sweden',
@@ -3737,7 +3771,7 @@ lib.make_purchase_sprites(
         'Z4p',
         s_d_Z4p_1_srj,
         s_d_Z4p_2_nklj,
-        #s_d_Z4p_3_ul, 
+        s_d_Z4p_3_donj, 
     ),
     s_d_Tp_1_sj,
     s_p_Co_1,
