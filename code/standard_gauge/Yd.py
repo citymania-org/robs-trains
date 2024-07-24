9c5bf27341d9b4390266b8058695b0f8ce434f1b
Gurk-meja <66508651+Gurk-meja@users.noreply.github.com>
2024-07-24 10:53:31 +0200

    Yd code (may work)

---
+++ code/standard_gauge/Yd.py
@@ -0,0 +1,47 @@
+import grf, lib
+
+from datetime import date
+
+from common import Train, colours, make_psd_cc_liveries, standard_gauge
+
+COMMON_Yd_PROPS = dict(
+    length=5,
+    misc_flags=Train.Flags.MULTIPLE_UNIT + Train.Flags.USE_2CC,
+    power_type='diesel',
+    engine_class=Train.EngineClass.DIESEL, 
+    track_type=standard_gauge,
+    max_speed=Train.kmhish(80),
+    power=130,
+    vehicle_life=30,
+    model_life=30,
+    climates_available=grf.ALL_CLIMATES,
+    weight=7,
+    tractive_effort_coefficient=80,
+    running_cost_factor=200,
+    cargo_capacity=24,
+    loading_speed=5,
+    cost_factor=25,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+    country='sweden',
+)
+
+s_e_Yd_1_sj = Train(
+    **COMMON_Yd_PROPS,
+    id='s_e_Yd_1_sj',
+    name='SJ Yd',
+    liveries=make_psd_cc_liveries(
+        'pp/1935_SE_Yd.psd',
+        shading='Yd',
+        paint='SJ',
+        overlay=('Lights', 'Details'),
+        cc_replace=colours["CREAM"],
+        cc2_replace=colours["CREAM"]
+    ),
+    company='na',
+    introduction_date=date(1935, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Operator': 'SJ',
+        'Use': 'Local trains, 3rd class',
+        'Builder': 'Hilding Carlsson'
+    }),
+)
