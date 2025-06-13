e21bf733132b4d6e86f622b99afc56e38b7d8898
Gurk-meja <66508651+Gurk-meja@users.noreply.github.com>
2025-06-14 01:54:01 +0200

    Type 64 backup

--- generate.py
+++ generate.py
@@ -143,11 +143,17 @@ from code.standard_gauge.ic3 import d_d_mf_1_dsb, d_d_mf_2_dsb, d_d_mf_3_dsb, d_
 
 # emu 15
 
+from code.standard_gauge.type64 import n_e_type64_1_nsb
+
 from code.standard_gauge.Xoa4 import s_e_Xoa4_1_sj, s_e_Xoa4_2_sj, s_e_Cox4_1_sj, s_e_Cox4_2_sj
 
 from code.standard_gauge.Xoa3 import s_e_Xoa3_1_bj, s_e_Xoa3_2_sj
 
+<<<<<<< Updated upstream
 from code.standard_gauge.X1 import s_e_X1_1_sj, s_e_X1_2_sl, s_e_X1_3_sl
+=======
+from code.standard_gauge.X1 import s_e_x1a_1_sj, s_e_x1a_2_sj, s_e_x1a_3_sj
+>>>>>>> Stashed changes
 
 from code.standard_gauge.type69 import n_o_BM69A_1_nsb, n_o_BM69A_2_nsb, n_o_BM69A_3_nsb
 
