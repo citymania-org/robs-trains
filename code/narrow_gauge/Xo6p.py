e763e4198e90e0d5530df088874b0972a0e0c7fe
Gurk-meja <66508651+Gurk-meja@users.noreply.github.com>
2025-07-04 20:41:42 +0200

    New vehicles to main
    They still need to be added to generate.py

---
+++ code/narrow_gauge/Xo6p.py
@@ -0,0 +1,47 @@
+import grf, lib
+
+from datetime import date
+
+from common import Train, Livery, extra_narrow_gauge, colours, make_psd_cc_liveries
+
+COMMON_Xo6p_PROPS = dict(
+    length=9,
+    misc_flags=Train.Flags.USE_2CC,
+    power_type='diesel',
+    engine_class=Train.EngineClass.DIESEL,
+    track_type=extra_narrow_gauge, 
+    max_speed=Train.kmhish(85),
+    power=440,
+    vehicle_life=30,
+    model_life=30,
+    climates_available=grf.ALL_CLIMATES,
+    weight=33,
+    tractive_effort_coefficient=80,
+    running_cost_factor=200,
+    cargo_capacity=72,
+    loading_speed= 10,
+    cost_factor=25, #balance later
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
+s_d_Xo6p_1_srj = Train(
+    **COMMON_Xo6p_PROPS,
+    id='s_d_Xo6p_1_srj',
+    name='SRJ 3110-3112 "Rydbergare"',
+    liveries=make_psd_cc_liveries(
+        'pp/Xo6p.psd', 
+        shading=['Xo6p', 'srjroof'], 
+        paint='srj', 
+        overlay='srjlights', 
+        cc_replace=colours["DCREAM"], 
+        cc2_replace=colours["SEBROWN"],
+    ),
+    country='sweden',
+    company='na',
+    introduction_date=date(1957, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Operator': 'SRJ',
+        'Use': 'Universal',
+        'Builder': 'ASJ'
+    }),
+)
---
+++ code/standard_gauge/type64.py
@@ -0,0 +1,47 @@
+import grf, lib
+
+from datetime import date
+
+from common import Train, colours, make_psd_cc_liveries, standard_gauge_15kv
+
+COMMON_type64_PROPS = dict(
+    length=8,
+    misc_flags=Train.Flags.MULTIPLE_UNIT + Train.Flags.USE_2CC,
+    power_type='15kv',
+    engine_class=Train.EngineClass.ELECTRIC, 
+    track_type=standard_gauge_15kv,
+    max_speed=Train.kmhish(50),
+    power=632,
+    vehicle_life=30,
+    model_life=30,
+    climates_available=grf.ALL_CLIMATES,
+    weight=35,
+    tractive_effort_coefficient=80,
+    running_cost_factor=200,
+    cargo_capacity=158,
+    loading_speed=10,
+    cost_factor=25,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+    country='norway',
+)
+
+n_e_type64_1_nsb = Train(
+    **COMMON_type64_PROPS,
+    id='n_e_type64_1_nsb',
+    name='NSB Type 64',
+    liveries=make_psd_cc_liveries(
+        'pp/type64.psd',
+        shading=['type64', 'type64roof'],
+        paint='type64nsb',
+        overlay=('type64light',),
+        cc_replace=colours["MAROON"],
+        cc2_replace=colours["YELLOW"]
+    ),
+    company='na',
+    introduction_date=date(1935, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Operator': 'NSB',
+        'Use': 'Local trains',
+        'Builder': 'Strømmen',
+    }),
+)
