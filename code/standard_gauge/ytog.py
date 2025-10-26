6d73c49fbe36e4ad28d5da0bf9ceb2e4b4efbf29
BL97 <thehectorlavoe@yahoo.co.uk>
2025-10-26 14:36:55 +0000

    code for more pigs

--- code/standard_gauge/ytog.py
+++ code/standard_gauge/ytog.py
@@ -84,6 +84,25 @@ d_d_y2_1_dsb = Train(
     }),
 )
 
+d_d_y2_2_dsb = Train(
+    id='d_d_y2_2_dsb',
+    **COMMON_y2_PROPS,
+    name='VLTJ YM Railbus "Fjorden"',
+    liveries=make_psd_cc_liveries(
+        'pp/9Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["RED"]
+    ),
+    country='denmark',
+    introduction_date=date(2000, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Short distance local passengers',
+        'Trivia': 'One locomotive bought from LNJ in 1999 #15, named Fjorden',
+    }),
+)
+
 COMMON_y3_PROPS = dict( # this is based on the LNJ YS 25
     length=9,
     misc_flags=Train.Flags.USE_2CC,
@@ -283,6 +302,68 @@ d_d_y6_1_dsb = Train(
     refittable_cargo_classes=grf.CargoClass.PASSENGERS,
 )
 
+d_d_y6_2_dsb = Train(
+    id='d_d_y6_2_dsb',
+    **COMMON_y6_PROPS,
+    name='VLTJ YM-YS',
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["CREAM"]
+    ),
+    country='denmark',
+    introduction_date=date(1983, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Local passengers',
+        'Trivia': 'Three locomotives bought in 1983 #12, #13, #14, named Storåen, Vigen and Tangen',
+    }),
+).add_articulated_part(
+    id='d_d_y62_2_dsb',
+    length=8,
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["CREAM"]
+    ),
+    cargo_capacity=1,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
+d_d_y6_3_dsb = Train(
+    id='d_d_y6_3_dsb',
+    **COMMON_y6_PROPS,
+    name='VLTJ YM-YS',
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["RED"]
+    ),
+    country='denmark',
+    introduction_date=date(1999, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Local passengers',
+        'Trivia': 'One more locomotive was bought from NJ in 2005 #16, named Heden',
+    }),
+).add_articulated_part(
+    id='d_d_y62_3_dsb',
+    length=8,
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["RED"]
+    ),
+    cargo_capacity=1,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
 COMMON_y7_PROPS = dict( # this is based on the OHJ YM 51
     length=8,
     misc_flags=Train.Flags.USE_2CC,
@@ -331,6 +412,68 @@ d_d_y7_1_dsb = Train(
         paint=('2',),
         cc_replace=colours["RED"],
         cc2_replace=colours["GREY1"]
+    ),
+    cargo_capacity=1,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
+d_d_y7_2_dsb = Train(
+    id='d_d_y7_2_dsb',
+    **COMMON_y7_PROPS,
+    name='VLTJ YM-YM',
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["CREAM"]
+    ),
+    country='denmark',
+    introduction_date=date(1983, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Local passengers',
+        'Trivia': 'Three locomotives bought in 1983 #12, #13, #14, named Storåen, Vigen and Tangen',
+    }),
+).add_articulated_part(
+    id='d_d_y72_2_dsb',
+    length=8,
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["CREAM"]
+    ),
+    cargo_capacity=1,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
+d_d_y7_3_dsb = Train(
+    id='d_d_y7_3_dsb',
+    **COMMON_y7_PROPS,
+    name='VLTJ YM-YM',
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["RED"]
+    ),
+    country='denmark',
+    introduction_date=date(1999, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Local passengers',
+        'Trivia': 'One more locomotive was bought from NJ in 2005 #16, named Heden',
+    }),
+).add_articulated_part(
+    id='d_d_y72_3_dsb',
+    length=8,
+    liveries=make_psd_cc_liveries(
+        'pp/8Template.psd',
+        shading=('1',),
+        paint=('2',),
+        cc_replace=colours["ORANGE"],
+        cc2_replace=colours["RED"]
     ),
     cargo_capacity=1,
     refittable_cargo_classes=grf.CargoClass.PASSENGERS,
