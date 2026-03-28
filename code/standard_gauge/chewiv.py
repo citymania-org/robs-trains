d23de1d6c69b16950b063cafaae4cd6ec96f5554
BL97 <thehectorlavoe@yahoo.co.uk>
2026-03-28 19:37:13 +0000

    update ew iv

--- code/standard_gauge/chewiv.py
+++ code/standard_gauge/chewiv.py
@@ -153,8 +153,26 @@ ch_p_ewiv_6 = Train(
     }),
 )
 
+COMMON_ews_PROPS = dict(
+    length=12,
+    misc_flags=Train.Flags.USE_2CC,
+    power_type='na',
+    engine_class=Train.EngineClass.DIESEL, 
+    track_type=standard_gauge,
+    power=1,
+    callbacks={'properties': {'power': 0},},
+    vehicle_life=30,
+    model_life=30,
+    climates_available=grf.ALL_CLIMATES,
+    weight=40,
+    tractive_effort_coefficient=80,
+    running_cost_factor=200,
+    cost_factor=25,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+)
+
 ch_p_ewiv_7 = Train(
-    **COMMON_ew_PROPS,
+    **COMMON_ews_PROPS,
     id='ch_p_ewiv_7',
     name='SBB Bt',
     liveries=make_psd_cc_liveries(
@@ -176,7 +194,7 @@ ch_p_ewiv_7 = Train(
 )
 
 ch_p_ewiv_8 = Train(
-    **COMMON_ew_PROPS,
+    **COMMON_ews_PROPS,
     id='ch_p_ewiv_8',
     name='SBB Bt',
     liveries=make_psd_cc_liveries(
@@ -198,7 +216,7 @@ ch_p_ewiv_8 = Train(
 )
 
 ch_p_ewiv_9 = Train(
-    **COMMON_ew_PROPS,
+    **COMMON_ews_PROPS,
     id='ch_p_ewiv_9',
     name='SBB Bt',
     liveries=make_psd_cc_liveries(
