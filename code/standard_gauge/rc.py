39bb6d43edab91e2e5334ce3f4e61a813eb7659d
BL97 <thehectorlavoe@yahoo.co.uk>
2026-05-13 19:19:25 +0100

    Rc3R

--- code/standard_gauge/rc.py
+++ code/standard_gauge/rc.py
@@ -70,7 +70,7 @@ s_e_rc1245_2_sj = Train(
 s_e_rc1245_3_sj = Train(
     id='s_e_rc1245_3_sj',
     **COMMON_rc_PROPS,
-    name='SJ Rc1/2', #this is the rc one
+    name='SJ Rc1R/2R', #this is an rc one
     liveries=make_psd_cc_liveries(
         'pp/rc.psd',
         shading=('rc',),
@@ -443,6 +443,27 @@ s_e_rc36_9_sj = Train(
     max_speed=Train.kmhish(160),
     weight=76,
     introduction_date=date(2021, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Universal',
+    }),
+)
+
+s_e_rc36_10_sj = Train(
+    id='s_e_rc36_10_sj',
+    **COMMON_rc_PROPS,
+    name='SJ Rc3R', #this is an rc one
+    liveries=make_psd_cc_liveries(
+        'pp/rc.psd',
+        shading=('rc',),
+        paint=('sj6',),
+        overlay=('light'),
+        cc_replace=colours["BLUE"],
+        cc2_replace=colours["RED"]
+    ),
+    company='na',
+    max_speed=Train.kmhish(160),
+    weight=76,
+    introduction_date=date(1999, 1, 1),
     additional_text=grf.fake_vehicle_info({
         'Use': 'Universal',
     }),
