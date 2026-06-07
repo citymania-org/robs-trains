0c9214fae063cc18b447d7de3fc72ac4c9b371d5
BL97 <thehectorlavoe@yahoo.co.uk>
2026-06-07 23:54:09 +0100

    65000 pt1

--- code/standard_gauge/frsmallseries.py
+++ code/standard_gauge/frsmallseries.py
@@ -4,6 +4,62 @@ from datetime import date
 
 from common import Train, colours, make_psd_cc_liveries, standard_gauge
 
+COMMON_65000_PROPS = dict(
+    length=9,
+    misc_flags=Train.Flags.USE_2CC,
+    power_type='diesel',
+    engine_class=Train.EngineClass.DIESEL, 
+    track_type=standard_gauge,
+    vehicle_life=30,
+    model_life=30,
+    climates_available=grf.ALL_CLIMATES,
+    tractive_effort_coefficient=80,
+    running_cost_factor=200,
+    cargo_capacity=1,
+    cost_factor=25,
+    refittable_cargo_classes=grf.CargoClass.PASSENGERS,
+    callbacks={'properties': {'cargo_capacity': 0},},
+    max_speed=Train.kmhish(130),
+    power=1849,
+    weight=112,
+)
+
+fr_d_65000_1 = Train(
+    **COMMON_65000_PROPS,
+    id='fr_d_65000_1',
+    name='SNCF 060 DB',
+    liveries=make_psd_cc_liveries(
+        'pp/Template.psd',
+        shading=('9a',),
+        paint=('9b',),
+        cc_replace=colours["BLUE"],
+        cc2_replace=colours["BLUE"]
+    ),
+    country='france',
+    company='na',
+    introduction_date=date(1956, 1, 1),
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Universal',
+        'Trivia': 'Later classed as CC 65000',
+    }),
+)
+
+fr_d_65000_2 = Train(
+    **COMMON_65000_PROPS,
+    id='fr_d_65000_2',
+    liveries=make_psd_cc_liveries(
+        'pp/Template.psd',
+        shading=('9a',),
+        paint=('9b',),
+        cc_replace=colours["BLUE"],
+        cc2_replace=colours["NAVY"]
+    ),
+    country='france',
+    company='na',
+    additional_text=grf.fake_vehicle_info({
+        'Use': 'Universal',
+    }),
+)
 
 COMMON_69000_PROPS = dict(
     length=9,
